f06d7cfc5a3f63950d16874d7224c3b548f05e72
Ростислав Луценко <poltavarost@gmail.com>
2019-07-17 21:13:11 +0300

    changed header and promo

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+document.querySelector('.burger').addEventListener('click', function() {
+    let nav = document.querySelector('nav');
+    this.classList.toggle('burger--close');
+
+    if (!nav.classList.contains('nav--active')) {
+        nav.style.display = 'flex';
+        setTimeout(function() {
+            nav.classList.add('nav--active');
+        }, 15);
+    } else {
+        nav.classList.remove('nav--active');
+        setTimeout(function() {
+            nav.style = '';
+        }, 250);
+    }
+});
